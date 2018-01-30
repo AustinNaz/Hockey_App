db8b5e41c01f543ee1cea67005f04e7e1ef45f7a
MistaTG <nazanator@hotmail.com>
2018-01-29 23:37:01 -0700

    baseline physics implementation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     public float moveSpeed;
     public Joystick joystick;
+    public Rigidbody rb;
 
     // Use this for initialization
-    void Start () {
-		
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();	
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Vector3 moveVector = (transform.right * joystick.Horizontal + transform.forward * joystick.Vertical).normalized;
         transform.Translate(moveVector * moveSpeed * Time.deltaTime);
+        float speed = moveSpeed * Time.deltaTime;
+        rb.AddForce(moveVector * speed);
     }
 }
