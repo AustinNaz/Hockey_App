99ad14c7a10ee4ed56732c23d54d1dca52b67001
MistaTG <nazanator@hotmail.com>
2018-01-29 20:57:14 -0700

    Added joystick movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    public float moveSpeed;
+    public Joystick joystick;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        Vector3 moveVector = (transform.right * joystick.Horizontal + transform.forward * joystick.Vertical).normalized;
+        transform.Translate(moveVector * moveSpeed * Time.deltaTime);
+    }
 }
